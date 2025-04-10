2d41cda9a47b56e1bbc9cbfc72f836272441ef12
Jonathanleivag <email@jonathanleivag.cl>
2025-04-10 05:56:08 -0400

    feat(orders): integrate Pusher for real-time cart updates
    Add Pusher integration to enable real-time updates for completed carts on the orders dashboard. This includes adding Pusher-related environment variables, installing the pusher-js dependency, and implementing the Pusher client in the orders page component.

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -4,18 +4,38 @@ import OrdersDashboardComponent from "@/components/dashboard/orders/orders.dashb
 import { withAuth } from "@/hoc/withAuth";
 import { useDataFetch } from "@/hooks/useDataFetch.hook";
 import { Card } from "@/type";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import Pusher from "pusher-js";
+import { getENV } from "@/config/env.config";
+import { ENV } from "@/enum";
 
 const Orders: FC = () => {
+  const [newCart, setNewCart] = useState<Card>({} as Card);
+
   const [carts, loading] = useDataFetch<Card[]>(
     "/cart/completed/user",
     false,
     0,
     0,
     true,
-    "",
+    newCart,
     []
   );
+
+  useEffect(() => {
+    const pusher = new Pusher(getENV(ENV.KEY_PUSHER), {
+      cluster: getENV(ENV.CLUSTER_PUSHER),
+    });
+    const channel = pusher.subscribe("completeCart");
+    channel.bind("complete-cart", (data: Card) => {
+      console.log("🚀 ~ channel.bind ~ data:", data);
+      setNewCart(data);
+    });
+    return () => {
+      pusher.unsubscribe("completeCart");
+    };
+  }, [newCart]);
+
   return (
     <DashboardLayout selected="Orders">
       {!loading && <OrdersDashboardComponent orders={carts} />}
